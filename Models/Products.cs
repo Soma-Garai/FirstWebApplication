a05fc3e0642dfa3606b4b86363921069a10173c5
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-04 17:11:33 +0530

    Update Products.cs

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,6 +1,21 @@
-﻿namespace FirstWebApplication.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstWebApplication.Models
 {
     public class Products
     {
+        public int product_id { get; set; }
+        public string? product_name { get; set;}
+        public string? product_description { get; set;}
+        public int? product_price { get; set; }
+        //public int product_Quantity { get; set; }
+
+        
+
+        //This property is for the form file upload, so it's not mapped to the database
+        [NotMapped]
+        public IFormFile? product_image { get; set; }
+        public string? product_ImagePath { get; set; }
     }
 }
