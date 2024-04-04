1a933d7ceba8ed3521aa31a856d1ba0f95c9294e
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-04 17:10:16 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,9 +22,23 @@ namespace FirstWebApplication.Controllers
         {
             return View();
         }
-        public IActionResult Chocolates()//done
+        
+        public IActionResult Chocolates()
         {
-            return View();
+         // Retrieve products from the database
+          List<ProductViewModel> allProducts = _appDbContext.tblProducts.ToList();
+
+         // Map ProductViewModel to Products
+          var products = allProducts.Select(p => new Products
+        {
+          product_id = p.product_id,
+          product_name = p.product_name,
+          product_description = p.product_description,
+          product_price = p.product_price,
+          product_ImagePath = p.product_ImagePath
+          }).ToList();
+          ViewBag.Products = products;
+          return View();
         }
         public IActionResult Testimonial()//done
         {
