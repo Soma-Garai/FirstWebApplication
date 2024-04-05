bf4e0c040f71d30f357fc2a855fc4b8f8b1809ee
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-05 17:34:45 +0530

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using FirstWebApplication.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApplication.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<ProductViewModel>? Products { get; set; } // Navigation property
+
+    }
+}
