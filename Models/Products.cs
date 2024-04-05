2b25d77d980f4b58a45185eb69a8f609b03b592a
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-05 17:45:56 +0530

    Update Products.cs

--- Models/Products.cs
+++ Models/Products.cs
@@ -11,11 +11,14 @@ namespace FirstWebApplication.Models
         public int? product_price { get; set; }
         //public int product_Quantity { get; set; }
 
-        
-
         //This property is for the form file upload, so it's not mapped to the database
         [NotMapped]
         public IFormFile? product_image { get; set; }
         public string? product_ImagePath { get; set; }
+
+        
+        public int CategoryId { get; set; }      // Foreign key
+        public Category? category { get; set; }  // Navigation property
+        public string? CategoryName { get; set; }
     }
 }
