161a5beb40476548b14e77bf435eb8192070f1bc
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-05 17:47:36 +0530

    Add files via upload

---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FirstWebApplication.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstWebApplication.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public int product_id { get; set; }
+        public string? product_name { get; set; }
+        public string? product_description { get; set; }
+        public int? product_price { get; set; }
+        public string? product_ImagePath { get; set; }
+        //[NotMapped]
+        //public Cart Cart { get; set; }
+
+
+        public int CategoryId { get; set; }      // Foreign key
+        public Category? category { get; set; }  // Navigation property
+    }
+}
---
+++ ViewModels/ProductWithCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FirstWebApplication.ViewModels
+{
+    public class ProductWithCategoryViewModel
+    {
+        public int product_id { get; set; }
+        public string? product_name { get; set; }
+        public string? product_description { get; set; }
+        public int? product_price { get; set; }
+        public string? product_ImagePath { get; set; }
+
+        // Category properties
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
