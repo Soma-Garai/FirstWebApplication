5318656bb9939ef633815efba7e330cbb5fd4111
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-09 18:39:19 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,17 +9,55 @@ var connectionString = builder.Configuration.GetConnectionString("FirstWebApplic
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FirstWebApplicationDbContext>();
-builder.Services.AddIdentity<UserModel, IdentityRole>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultUI()
             .AddDefaultTokenProviders();
-builder.Services.AddScoped<UserManager<UserModel>>();
-builder.Services.AddScoped<SignInManager<UserModel>>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = false;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+
+    options.LoginPath = "/User/Login"; // Customize your login path
+    options.AccessDeniedPath = "/User/AccessDenied"; // Customize your access denied path
+    options.SlidingExpiration = true;
+});
+builder.Services.AddScoped<UserManager<IdentityUser>>();
+builder.Services.AddScoped<SignInManager<IdentityUser>>();
 builder.Services.AddScoped<Products>();
 //builder.Services.AddScoped<Cart>();
 builder.Services.AddScoped<Category>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add session support
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".MyApp.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Adjust as needed
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
@@ -36,7 +74,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession(); // Use session middleware
 app.UseAuthorization();
 
 app.MapControllerRoute(
