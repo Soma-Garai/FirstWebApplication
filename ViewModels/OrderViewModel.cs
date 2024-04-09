8faa650da939e7d18c0c0e0e7d044a6adf73aa07
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-09 18:29:24 +0530

    Add files via upload

---
+++ ViewModels/OrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstWebApplication.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int UserId { get; set; } // User placing the order
+        public List<OrderItemViewModel> OrderItems { get; set; } // List of items in the order
+
+        public OrderViewModel()
+        {
+            OrderItems = new List<OrderItemViewModel>();
+        }
+    }
+
+    public class OrderItemViewModel
+    {
+        public int ProductId { get; set; } // Id of the product
+        public int Quantity { get; set; } // Quantity of the product in the order
+        public int? TotalPrice { get; set; }
+    }
+
+}
