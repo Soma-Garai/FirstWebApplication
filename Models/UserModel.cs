e73ef2d11c7e38b021c6657717f274ae3c5ff315
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-11 18:31:47 +0530

    Update UserModel.cs

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
 namespace FirstWebApplication.Models
 {
     public class UserModel : IdentityUser
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string UserId { get; set; }
+        //public string UserName { get; set; }
+        //public string Email { get; set; }
+        //public string Password { get; set; }
     }
 }
