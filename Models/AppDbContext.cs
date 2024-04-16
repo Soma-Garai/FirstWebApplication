d19523ef9e121737335b02c49e55f54cbcfecbe9
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-16 11:11:12 +0530

    Update AppDbContext.cs

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -14,11 +14,26 @@ public class AppDbContext : IdentityDbContext<UserModel>
     //public DbSet<UserModel> tblUsers { get; set; }
     public DbSet<ProductViewModel> tblProducts { get; set; }
     public DbSet<Category> tblCategories { get; set; }
-    //public DbSet<CartItem> Carts { get; set; } // DbSet for CartItems
-
+    public DbSet<Orders> tblOrders { get; set; }
+    public DbSet<OrderDetails> tblOrderDetails { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        //// Configure the relationship
+        //modelBuilder.Entity<OrderDetails>()
+        //    .HasOne(od => od.Orders)            // OrderDetails has one Order
+        //    .WithMany(o => o.OrderDetails)   // Order has many OrderDetails
+        //    .HasForeignKey(od => od.OrderId); // Foreign key is OrderId
+
+        modelBuilder.Entity<IdentityUserLogin<string>>()
+            .HasKey(l => new { l.LoginProvider, l.ProviderKey });
+        modelBuilder.Entity<IdentityUserRole<string>>(userRole =>
+        {
+            userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+        });
+        modelBuilder.Entity<IdentityUserToken<string>>(userToken =>
+        {
+            userToken.HasKey(ut => new { ut.UserId, ut.LoginProvider, ut.Name });
+        });
 
         // Seed categories
         modelBuilder.Entity<Category>().HasData(
@@ -26,7 +41,8 @@ public class AppDbContext : IdentityDbContext<UserModel>
             new Category { CategoryId = 2, CategoryName = "Milk Chocolate" },
             new Category { CategoryId = 3, CategoryName = "White Chocolate" },
             new Category { CategoryId = 4, CategoryName = "Candy" }
-            // Add more categories as needed
+            
+
         );
     }
 
