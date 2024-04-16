0fbbec5be41cf3b632a3b8e193d37dc2a80bb86e
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-16 11:16:23 +0530

    Update OrderDetails.cs

--- Models/OrderDetails.cs
+++ Models/OrderDetails.cs
@@ -1,4 +1,9 @@
+using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FirstWebApplication.Models
 {
@@ -6,15 +11,18 @@ namespace FirstWebApplication.Models
     {
         [Key]
         public int OrderItemId { get; set; } // Primary Key 
-        public int OrderId { get; set; }    //[Foreign Key to tblOrders]
+
+            //[Foreign Key to tblOrders]
         public int ProductId { get; set; } // [Foreign Key to tblProducts]
         public string? ProductName { get; set; }
         public int? Quantity { get; set; }
         public int? product_price { get; set; }
         public int? TotalPrice  { get; set; }
 
-    // Navigation property
-    public Orders? Orders { get; set; }
+        [ForeignKey("Orders")]
+        public int OrderId { get; set; }
+        // Navigation property
+        public Orders? Orders { get; set; }
         //public Products? Products { get; set; }
         
         
