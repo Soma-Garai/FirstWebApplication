fd41c6e8729373096d37ef7e1ba58252fc9789e8
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-16 11:12:11 +0530

    Update CartItem.cs

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,6 +1,12 @@
-﻿namespace FirstWebApplication.Models
+using FirstWebApplication.ViewModels;
+
+namespace FirstWebApplication.Models
 {
     public class CartItem
     {
+        public ProductViewModel Product { get; set; } // The product in the cart
+        public int Quantity { get; set; }    // Quantity of the product
     }
+
 }
+
