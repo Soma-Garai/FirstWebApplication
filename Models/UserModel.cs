25af797c51a47145dc263dbc0a9b4619a1b02cfe
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-16 11:17:32 +0530

    Update UserModel.cs

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -8,7 +8,7 @@ namespace FirstWebApplication.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public string UserId { get; set; }
+        //public string UserId { get; set; }
         //public string UserName { get; set; }
         //public string Email { get; set; }
         //public string Password { get; set; }
