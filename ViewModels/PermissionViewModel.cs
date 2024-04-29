c20533f16444dc9b4517f9fc942820db15cbb38b
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-04-29 18:33:15 +0530

    Add files via upload

---
+++ ViewModels/PermissionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FirstWebApplication.ViewModels
+{
+    public class PermissionViewModel
+    {
+        public RoleManager<IdentityRole> RoleManager { get; set; }
+        public string RoleId { get; set; }
+        public IList<RoleClaimsViewModel> RoleClaims { get; set; }//Represents a list of RoleClaimsViewModel objects,
+                                                                  //each representing a permission associated with the role.
+    }
+    //This class represents a view model for a single permission associated with a role.
+    public class RoleClaimsViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
