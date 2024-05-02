5f1559b1c0f00c52c63662af5bdeaf27dfde0b73
Soma-Garai <156603054+Soma-Garai@users.noreply.github.com>
2024-05-02 17:52:03 +0530

    Update UserController.cs

--- Controllers/Controllers/UserController.cs
+++ Controllers/Controllers/UserController.cs
@@ -4,20 +4,24 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FirstWebApplication.ViewModels;
 using System.Net.Mail;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FirstWebApplication.Controllers
 {
+    [AllowAnonymous]
     public class UserController : Controller
     {
         private readonly SignInManager<UserModel> _signInManager;
         private readonly UserManager<UserModel> _userManager;
         private readonly AppDbContext _appDbContext;
+        private readonly IHttpContextAccessor _contextAccessor;
 
-        public UserController(SignInManager<UserModel> signInManager, UserManager<UserModel> userManager, AppDbContext appDbContext)
+        public UserController(SignInManager<UserModel> signInManager, UserManager<UserModel> userManager, AppDbContext appDbContext, IHttpContextAccessor contextAccessor)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _appDbContext = appDbContext;
+            _contextAccessor = contextAccessor;
         }
 
         [HttpGet]
@@ -104,9 +108,25 @@ namespace FirstWebApplication.Controllers
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
+            // Get the user identifier (e.g., user ID or username)
+            string userId = User.Identity.Name;
+
+            // Clear the cart data associated with the user's identifier from session
+            _contextAccessor.HttpContext.Session.Remove(userId);
+
+            // Perform logout operation
             await _signInManager.SignOutAsync();
+
+            // Redirect the user to the home page or login page
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+
+            return View();
+        }
     }
 }
